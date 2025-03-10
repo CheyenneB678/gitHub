4ae39dbe0bbc41b7c64e05eea05d13e81b6cd1c0
buchert70427 <buchert70427@B124-19>
2025-03-10 10:17:34 -0400

    Added additional commentary

--- gitHub/helloGit.cpp
+++ gitHub/helloGit.cpp
@@ -1,10 +1,16 @@
+/***
+*Version 1.03
+****/
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
 
-	cout << "Hello world.";
+	cout << "Hello world." << endl;
+	cout << "Additional commentary." << endl;
+	cout << "bla bla bla";
 	cin.get();
 	return 0;
 }
